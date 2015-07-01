12633784cf8eeafaceb287982264793835e783b6
Jonathan Heusser <jonathan.heusser@gmail.com>
2015-07-01 10:42:31 +0100

    Remove timestamp calculation for Bitfinex orderbook

--- xchange-bitfinex/src/main/java/com/xeiam/xchange/bitfinex/v1/BitfinexAdapters.java
+++ xchange-bitfinex/src/main/java/com/xeiam/xchange/bitfinex/v1/BitfinexAdapters.java
@@ -68,7 +68,7 @@ public final class BitfinexAdapters {
     OrdersContainer asksOrdersContainer = adaptOrders(btceDepth.getAsks(), currencyPair, OrderType.ASK);
     OrdersContainer bidsOrdersContainer = adaptOrders(btceDepth.getBids(), currencyPair, OrderType.BID);
 
-    return new OrderBook(new Date(Math.max(asksOrdersContainer.getTimestamp(), bidsOrdersContainer.getTimestamp())),
+    return new OrderBook(null,
         asksOrdersContainer.getLimitOrders(), bidsOrdersContainer.getLimitOrders());
   }
 
